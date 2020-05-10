3e8679c8f08f655b4f0a2c4814de58af7c71c756
nischalhp <nischal.hp@gmail.com>
2020-05-10 15:34:55 +0200

    Adding roles datamodel

--- guby_initialize.sh
+++ guby_initialize.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 python manage.py makemigrations
 python manage.py migrate
+python manage.py makemigrations roles
 python create_superuser.py 
 exec python manage.py runserver 0.0.0.0:5000

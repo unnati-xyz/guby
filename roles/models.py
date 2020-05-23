4617582ac49f24e2e7575339da66169bb98d7435
nischalhp <nischal.hp@gmail.com>
2020-05-23 10:17:03 +0200

    Removing roles app

--- roles/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Roles(models.Model):
-    id = models.IntegerField(primary_key=True, db_column='id', auto_created=True)
-    role_name = models.TextField(unique=True, db_column='role_name')

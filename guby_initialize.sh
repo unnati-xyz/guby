f0612c882d97e2fb4846d15a9295502d66a585f8
nischalhp <nischal.hp@gmail.com>
2020-05-23 10:19:22 +0200

    Using environment variables to trigger db

--- guby_initialize.sh
+++ guby_initialize.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
-python manage.py makemigrations
-python manage.py migrate
-python manage.py makemigrations roles
-python create_superuser.py 
+echo $INITIALIZE_DB
+echo $INITIALIZE_APP_DB
+if [ $INITIALIZE_DB == true ]
+then
+    echo "Initializing database for guby"
+    python manage.py makemigrations 
+    python manage.py migrate
+    python create_superuser.py 
+fi
+
+if [ $INITIALIZE_APP_DB == true ]
+then 
+    echo "Initializing application database for guby"
+    python manage.py makemigrations guby_backend 
+    python manage.py migrate guby_backend 
+
+fi
+
 exec python manage.py runserver 0.0.0.0:5000
